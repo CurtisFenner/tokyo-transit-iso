57c8627ded7a26c2d193803a98a7575aa6a95b6b
Curtis Fenner <3289892+CurtisFenner@users.noreply.github.com>
2024-11-07 13:04:00 +0900

    Render place list in details view

--- src/main.tsx
+++ src/main.tsx
@@ -137,40 +137,95 @@ async function generateInvertedIsoline(
 }
 
 type PlaceState = {
+	title: string,
 	coordinate: Coordinate,
 	weight: number,
 };
 
 function encodePlaceState(place: PlaceState): string {
-	return `${place.coordinate.lon.toFixed(6)}_${place.coordinate.lat.toFixed(6)}_w${place.weight}`;
+	return `${place.coordinate.lon.toFixed(6)}_${place.coordinate.lat.toFixed(6)}_w${place.weight}_t${place.title}`;
 }
 
 function decodePlaceState(place: string): PlaceState | null {
-	const match = place.match(/^([0-9.-]+)_([0-9.-]+)_w([0-9]+)$/);
+	const match = place.match(/^([0-9.-]+)_([0-9.-]+)_w([0-9]+)_t$(.*)$/);
 	if (match === null) {
 		return null;
 	}
+	const [_, lonStr, latStr, weightStr, titleStr] = match;
 	return {
 		coordinate: {
-			lon: parseFloat(match[1]),
-			lat: parseFloat(match[2]),
+			lon: parseFloat(lonStr),
+			lat: parseFloat(latStr),
 		},
-		weight: parseFloat(match[3]),
+		weight: parseFloat(weightStr),
+		title: titleStr,
 	};
-}
-
-function encodePlaceStates(places: PlaceState[]): string {
-	return places.map(encodePlaceState).join("..");
-}
-
-function decodePlaceStates(hash: string): PlaceState[] {
-	return hash.split("..").map(decodePlaceState).filter(x => x !== null);
 }
 
 const markers: {
 	state: PlaceState,
 	marker: maplibregl.Marker,
+	/**
+	 * The container in the "place-list-div" element.
+	 */
+	row: HTMLElement,
 }[] = [];
+
+const placeListDiv = document.getElementById("place-list-div")!;
+
+function addPlaceToState(
+	state: PlaceState,
+	rerenderIsolines: () => void,
+): void {
+	const marker = new maplibregl.Marker({ draggable: true })
+		.setLngLat(state.coordinate)
+		.addTo(map);
+
+	marker.on("dragend", async () => {
+		const selected = marker.getLngLat();
+		const newCoordinate = { lat: selected.lat, lon: selected.lng };
+		const me = markers.find(x => x.marker === marker);
+		if (me) {
+			me.state = {
+				...me.state,
+				coordinate: newCoordinate,
+			};
+		}
+
+		rerenderIsolines();
+	});
+
+	const row = document.createElement("div");
+	row.style.display = "flex";
+
+	const titleInput = document.createElement("input");
+	titleInput.classList.add("blending", "padded", "dotted");
+	titleInput.value = state.title;
+	titleInput.style.fontWeight = "bold";
+	titleInput.style.flex = "1 1 0";
+	row.appendChild(titleInput);
+
+	const trashButton = document.createElement("button");
+	trashButton.classList.add("blending");
+	trashButton.style.fontSize = "1.5em";
+	trashButton.style.lineHeight = "1em";
+	trashButton.textContent = "🗑️";
+	trashButton.onclick = () => {
+		markers.splice(0, markers.length, ...markers.filter(m => m.marker !== marker));
+		row.parentElement?.removeChild(row);
+		marker.remove();
+		rerenderIsolines();
+	};
+	row.appendChild(trashButton);
+
+	placeListDiv.appendChild(row);
+
+	markers.push({
+		state: state,
+		marker,
+		row,
+	});
+}
 
 async function main() {
 	const loadTimeline = new timer.Timeline();
@@ -324,10 +379,11 @@ async function main() {
 
 	function rerenderIsolines() {
 		const origins = markers.map(x => x.state);
-		const encodedState = encodePlaceStates(origins);
-		const searchParams = new URLSearchParams({
-			p: encodedState,
-		}).toString();
+		const encodedStates = origins.map(encodePlaceState);
+		const searchParams = new URLSearchParams(encodedStates.length === 1
+			? { p: encodedStates[0] }
+			: Object.fromEntries(encodedStates.map((value, i) => [`p${i + 1}`, value]))
+		).toString();
 		window.history.pushState({}, "", new URL("?" + searchParams, window.location.href));
 
 		shadelayers.recalculateSourceGeometry("basic", {
@@ -348,38 +404,24 @@ async function main() {
 		}
 	}
 
-	const urlPlaceStates =
-		decodePlaceStates(new URL(window.location.href).searchParams.get("p") || "");
-	if (urlPlaceStates.length === 0) {
-		urlPlaceStates.push({
-			coordinate: HACHIKO_COORDINATES,
-			weight: 1,
-		});
-	}
+	const queryParameters = new URL(window.location.href).searchParams;
+	const placeParameters = [...queryParameters]
+		.filter(([key]) => /^p[0-9]*/.test(key))
+		.map(([_, value]) => value);
+	const urlPlaceStates = placeParameters
+		.map(decodePlaceState)
+		.flatMap(x => x ? [x] : []);
 
 	for (const urlPlaceState of urlPlaceStates) {
-		const marker = new maplibregl.Marker({ draggable: true })
-			.setLngLat(urlPlaceState.coordinate)
-			.addTo(map);
+		addPlaceToState(urlPlaceState, rerenderIsolines);
+	}
 
-		marker.on("dragend", async () => {
-			const selected = marker.getLngLat();
-			const newCoordinate = { lat: selected.lat, lon: selected.lng };
-			const me = markers.find(x => x.marker === marker);
-			if (me) {
-				me.state = {
-					coordinate: newCoordinate,
-					weight: me.state.weight,
-				};
-			}
-
-			rerenderIsolines();
-		});
-
-		markers.push({
-			state: urlPlaceState,
-			marker,
-		});
+	if (urlPlaceStates.length === 0) {
+		addPlaceToState({
+			title: "Hachiko",
+			coordinate: HACHIKO_COORDINATES,
+			weight: 1,
+		}, rerenderIsolines);
 	}
 
 	rerenderIsolines();
